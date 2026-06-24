6e5f7f813453ef43802ecbaee9e206a78cf8d64b
amna-ctrl102 <rajpootamna393@gmail.com>
2026-06-25 00:25:23 +0500

    Complete Sponsored And Footer Section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,7 +3,9 @@ import Hero from "../components/Route/Hero/Hero";
 import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
-import Events from "../components/Events/Events.jsx";
+import Events from "../components/Events/Events";
+import Sponsored from "../components/Route/Sponsored/Sponsored";
+import Footer from "../components/layout/Footer";
 
 const HomePage = () => {
   return (
@@ -14,6 +16,8 @@ const HomePage = () => {
       <BestDeals/>
       <Events/>
       <FeaturedProduct/>
+      <Sponsored/>
+      <Footer/>
     </div>
   )
 }
