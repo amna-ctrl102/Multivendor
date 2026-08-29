d2216cd607a76bfdef8d46ef7c439419c20a81d8
amna-ctrl102 <rajpootamna393@gmail.com>
2026-08-29 18:05:18 +0500

    Fix Vercel serverless deployment

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,7 @@
-const app=require("./app");
+const app = require("./app");
 const connectDatabase = require("./db/database");
 
-//Handling uncaught Exception
-process.on("uncaughtException",(err)=>{
-    console.log(`Error: ${err.message}`);
-    console.log(`Shutting down the server for handling uncaught Exception`);
-})
-
-//config
-if(process.env.NODE_ENV !== "PRODUCTION"){
-    require("dotenv").config({
-        path: `${__dirname}/config/.env`
-    })
-}
-
-//Connect MongoDB
+// Connect MongoDB
 connectDatabase();
 
-//Create server
-const server=app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on http://localhost:${process.env.PORT}`)
-})
-
-//unhandled promise rejection
-process.on("unhandledRejection",(err)=>{
-    console.log(`Shutting down the server for ${err.message}`);
-    console.log(`Shutting down the server for unhandled promise rejection`);
-
-    server.close(()=>{
-        process.exit(1);
-    })
-})
+module.exports = app;
